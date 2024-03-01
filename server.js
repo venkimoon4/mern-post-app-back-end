af531537f0cb5039c8e9a0f30c9e7c6910b299d3
venkimoon4 <venkimoon4@gmail.com>
2024-03-01 20:28:43 +0530

    Second_Version

--- server.js
+++ server.js
@@ -2,12 +2,16 @@ import express from "express"
 import db from "./db.js";
 import router from "./Routes/PostRoutes.js";
 import userRouter from "./Routes/UserRoutes.js";
-
+import cors from "cors";
+import bodyParser from "body-parser";
 const app=express();
 
-app.use(express.json());
-app.use('/api/posts',router)
+app.use(cors())
+// app.use(express.json());
+app.use(bodyParser.json());
 
+
+app.use('/api/posts',router)
 app.use('/api/users',userRouter)
 
 app.listen(4000,()=>{
